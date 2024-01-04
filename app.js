83846b12096b3fac7134daf6a48f2086dea24670
Uladzislava Santalava <santalova.lada25@gmail.com>
2024-01-04 21:16:44 +0100

    fix: fix the display of completed items

--- app.js
+++ app.js
@@ -122,12 +122,13 @@ var deleteTask=function(){
 //Mark task completed
 var taskCompleted=function(){
     console.log("Complete Task...");
-
     //Append the task list item to the #completed-tasks
     var listItem=this.parentNode;
+    let label = listItem.querySelector("label");
+    label.className = "completed__list-label list-label task";
     completedTasksHolder.appendChild(listItem);
     bindTaskEvents(listItem, taskIncomplete);
-
+    
 }
 
 
@@ -137,6 +138,8 @@ var taskIncomplete=function(){
     //When the checkbox is unchecked
     //Append the task list item to the #incompleteTasks.
     var listItem=this.parentNode;
+    let label = listItem.querySelector("label");
+    label.className = "incomplete__list-label list-label task";
     incompleteTaskHolder.appendChild(listItem);
     bindTaskEvents(listItem,taskCompleted);
 }
